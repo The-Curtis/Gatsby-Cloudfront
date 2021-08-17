1d39422f397599d6edf2c60e2b1287ced3ef7020
Curtis Reisinger <reisingerrc@vcu.edu>
2021-08-17 00:21:22 -0400

    fixing

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,18 +4,4 @@ module.exports = {
     title: "Gatsby Site",
   },
   plugins: ["gatsby-plugin-styled-components"],
-  {
-    resolve: 'gatsby-source-dynamodb',
-    options: {
-      typeName: 'listStarbaseAHdataTables',
-      accessKeyId: '${{ secrets.AWS_DB_ACCESS_KEY_ID }}', 
-      secretAccessKey: '${{ secrets.AWS_DB_SECRET_ACCESS_KEY }}',
-      region: '${{ secrets.AWS_REGION',
-      params: {
-        TableName : "starbaseAHdataTable",
-        // OTHER PARAMS HERE
-      }
-    }
-  }
-  
 };
